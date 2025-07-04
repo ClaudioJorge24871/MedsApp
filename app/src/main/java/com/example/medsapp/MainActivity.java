34be035c98dbff76fc93835a0fb01d0de1b0a12a
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-07-04 19:18:16 +0100

    feat: UI elements for testing

--- app/src/main/java/com/example/medsapp/MainActivity.java
+++ app/src/main/java/com/example/medsapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.medsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //----------------UI Elements----------------------//
+    private EditText editTextTopic;
+    private Button btnSubscribe;
+
+
+    //----------------MQTT Elements----------------------//
     private static final String BROKER_URL = "tcp://192.168.1.186:1883";    // Broker URl for connection
     private static final String CLIENT_ID = "claudRPI";                     // Client Id
     private MQTTHandler mqttHandler;                                        // Mqtt object
@@ -23,10 +30,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         mqttHandler = new MQTTHandler();
         mqttHandler.connect(BROKER_URL,CLIENT_ID);
+
+        editTextTopic = findViewById(R.id.editTextText2);
+        btnSubscribe = findViewById(R.id.PubTopicbutton);
+
         // In the future, subscribes to all topics set by the raspberry
         //mqttHandler.subscribe(topico);
+
+        // Button listener
+        btnSubscribe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String message = editTextTopic.getText().toString().trim();
+                if(!message.isEmpty()){
+                    publishMessage("testMqtt/topic",message);
+                }
+            }
+        });
     }
 
     /**
@@ -35,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
      * @param message
      */
     private void publishMessage(String topic, String message){
+        Toast.makeText(this, "Publishing message:" + message, Toast.LENGTH_SHORT).show();
         mqttHandler.publish(topic,message);
     }
 
