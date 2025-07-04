d75a535c7af319377f1f9fc0f424c1a1668ec87b
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-07-04 18:17:39 +0100

    feat: Mqtt handler integration on main app

--- app/src/main/java/com/example/medsapp/MainActivity.java
+++ app/src/main/java/com/example/medsapp/MainActivity.java
@@ -10,15 +10,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String BROKER_URL = "tcp://192.168.1.186:1883";    // Broker URl for connection
+    private static final String CLIENT_ID = "claudRPI";                     // Client Id
+    private MQTTHandler mqttHandler;                                        // Mqtt object
+
+    /**
+     * On app creation
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mqttHandler = new MQTTHandler();
+        mqttHandler.connect(BROKER_URL,CLIENT_ID);
+        // In the future, subscribes to all topics set by the raspberry
+        //mqttHandler.subscribe(topico);
+    }
+
+    /**
+     * Publish a message topic
+     * @param topic
+     * @param message
+     */
+    private void publishMessage(String topic, String message){
+        mqttHandler.publish(topic,message);
+    }
+
+    /**
+     * Method to subscribe to a topic
+     * @param topic
+     */
+    private void subscribeToTopic(String topic){
+        mqttHandler.subscribe(topic);
+    }
+
+
+    /**
+     * On destroying the instance of the app
+     */
+    @Override
+    protected void onDestroy() {
+        mqttHandler.disconnect(); //disconnects the mqtt instance before destroying the app
+        super.onDestroy();
     }
 }
