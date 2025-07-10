e63c5561110706e4b5c750c451ae7d860d78fcbb
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-07-10 14:03:38 +0100

    feat: prepare the button to add a network or save the device

--- app/src/main/java/com/example/medsapp/AddNewDevice.java
+++ app/src/main/java/com/example/medsapp/AddNewDevice.java
@@ -32,32 +32,38 @@ public class AddNewDevice extends AppCompatActivity {
         deviceTitle = findViewById(R.id.tituloText);
         deviceDescription = findViewById(R.id.desText);
 
-        addDeviceButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                // When the button to add a new device is clicked. Store the new device
-                //Gets the device info
-                String id = deviceId.getText().toString().trim();
-                String title = deviceTitle.getText().toString().trim();
-                String description = deviceDescription.getText().toString().trim();
 
-                if(id.isEmpty() || title.isEmpty()){
-                    deviceId.setError("Required");
-                    deviceId.setError("Required");
-                    return;
+        if (addDeviceButton.getText().equals("Guardar")){
+            addDeviceButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    // When the button to add a new device is clicked. Store the new device
+                    //Gets the device info
+                    String id = deviceId.getText().toString().trim();
+                    String title = deviceTitle.getText().toString().trim();
+                    String description = deviceDescription.getText().toString().trim();
+
+                    if(id.isEmpty() || title.isEmpty()){
+                        deviceId.setError("Required");
+                        deviceId.setError("Required");
+                        return;
+                    }
+
+                    // Stores the new device
+                    Device newDevice = new Device(id, title, description);
+                    DeviceStorage.addDevice(AddNewDevice.this, newDevice);
+
+                    //Back to the home page
+                    Intent intent = new Intent(AddNewDevice.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
                 }
+            });
+        }else{ // Add a network
+            // Open the browser to insert the SSID and Password of the desired network
+        }
 
-                // Stores the new device
-                Device newDevice = new Device(id, title, description);
-                DeviceStorage.addDevice(AddNewDevice.this, newDevice);
-
-                //Back to the home page
-                Intent intent = new Intent(AddNewDevice.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-
-            }
-        });
         // Go back arrow
         ImageView backIcon = findViewById(R.id.backIcon);
         backIcon.setOnClickListener(v -> finish());
